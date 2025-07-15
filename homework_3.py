fe2de65ad5425f70f441b84d846ffb92d7bae74a
Isachenko Egor <bogdan1414148@gmail.com>
2025-07-15 11:30:15 +0300

    дз№3 - выполнено

---
+++ homework_3.py
@@ -0,0 +1,44 @@
+class Person:
+    def __init__(self,name,birth_date,occupation,higher_education=False):
+        self.name = name
+        self.birth_date = birth_date
+        self.__occupation = occupation
+        self.__higher_education = higher_education
+
+    def get_occupation(self):
+        return self.__occupation
+
+    def get_higher_education(self):
+        return self.__higher_education
+
+
+    def introduce(self):
+        print(f'Привет меня зовут {self.name}, я родился {self.birth_date}, я {self.get_occupation()}' )
+
+class Classmate(Person):
+    def __init__(self,name,birth_date,occupation,higher_education,group_name):
+        super().__init__(name, birth_date, occupation, higher_education)
+        self.group_name = group_name
+    def introduce(self):
+        print(f'Привет меня зовут {self.name}, я одноклассник Егора , учились в {self.group_name},  я родился {self.birth_date}, я {self.get_occupation()} , Высшее образование -  {self.get_higher_education()}')
+
+
+class Friend(Person):
+    def __init__(self,name,birth_date,occupation,higher_education,hobby):
+        super().__init__(name, birth_date, occupation, higher_education)
+        self.hobby = hobby
+
+    def introduce(self):
+        print(f'Привет меня зовут {self.name}, я друг Егора ,  родился {self.birth_date}, я {self.get_occupation()} и я увлекаюсь {self.hobby} , Высшее образование -  {self.get_higher_education()}')
+
+
+
+
+amirkhan = Classmate(name='Амирхан', birth_date="07.01.2008", occupation='студент',higher_education=False,group_name= '11A' )
+adelya = Classmate(name='Аделя', birth_date='28.05.2009', occupation='студентка',higher_education=False ,group_name= '11A' )
+emir = Friend(name="Эмир", birth_date= '18.03.2000', occupation='Преподаватель', higher_education=True,hobby='Волейболом' )
+chingiz = Friend(name="Чынгыз", birth_date='20.10.2001', occupation="Консультант", higher_education=True, hobby='Играми')
+
+names = [amirkhan, adelya, emir, chingiz]
+for person in names:
+    person.introduce()
