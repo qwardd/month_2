1be98df6fa6ffe4fe88f43763e822c0952d6eeb4
Isachenko Egor <bogdan1414148@gmail.com>
2025-07-24 23:20:36 +0300

    выполнил дз№6

--- homeworks/homework_6.py
+++ homeworks/homework_6.py
@@ -1,8 +1,11 @@
 from blessed import Terminal
+
 term = Terminal()
 
-
-print(term.red('Красный'))
-print(term.green('Зелёный'))
-print(term.yellow('Жёлтый'))
-print(term.blue('Синий'))
+print(term.red3('apple'))
+print(term.yellow3('banana'))
+print(term.darkred('cherry'))
+print(term.plum('grape'))
+print(term.yellow3("mango"))
+print(term.darkorange("orange"))
+print(term.chocolate("peach"))
